900aa74333c5ac5ff7a884c68187b07e82d92fe0
Scott Davis <scottd@collegenet.com>
2018-07-04 15:16:08 -0700

    sanity check

---
+++ src/main/java/io/github/scottleedavis/mattermostintegrations/MattermostIntegrationsApplication.java
@@ -0,0 +1,12 @@
+package io.github.scottleedavis.mattermostintegrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattermostIntegrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattermostIntegrationsApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/scottleedavis/mattermostintegrations/MattermostIntegrationsApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.scottleedavis.mattermostintegrations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MattermostIntegrationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
