314c36e0c3ff42a70fd8f0366e9d6042e1bf2ca6
Scott Davis <scottd@collegenet.com>
2018-07-19 21:57:32 -0700

    fixed bugs

---
+++ application/src/main/java/io/github/scottleedavis/mattermost/integrations/AppContext.java
@@ -0,0 +1,24 @@
+package io.github.scottleedavis.mattermost.integrations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AppContext extends WebMvcConfigurationSupport {
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
+
+    // Of course , you can define the Executor too
+    @Bean
+    public Executor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+}
--- application/src/main/java/io/github/scottleedavis/mattermost/integrations/MattermostIntegrationsApplication.java
+++ application/src/main/java/io/github/scottleedavis/mattermost/integrations/MattermostIntegrationsApplication.java
@@ -3,14 +3,28 @@ package io.github.scottleedavis.mattermost.integrations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication(scanBasePackages = {"io.github.scottleedavis.mattermost"})
 @EnableJpaRepositories("io.github.scottleedavis.mattermost")
 @EntityScan("io.github.scottleedavis.mattermost")
+@EnableScheduling
 public class MattermostIntegrationsApplication {
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(MattermostIntegrationsApplication.class, args);
     }
 }
+
+
